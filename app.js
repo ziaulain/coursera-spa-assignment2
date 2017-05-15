ae47cdecea3c2abe10100d68836e81a05165b772
Zia ul ain <m.ziaulain@gmail.com>
2017-05-15 21:56:55 +0500

    inject was missing

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
-angular.module('ShoppingListCheckOff', [])
-  .controller('ToBuyController', function(ShoppingListCheckOffService) {
+(function () {
+
+	'use strict';
+
+  angular.module('ShoppingListCheckOff', [])
+  .controller('ToBuyController',ToBuyController)
+  .controller('AlreadyBoughtController', AlreadyBoughtController)
+  .service('ShoppingListCheckOffService', ShoppingListCheckOffService);
+
+
+  ToBuyController.$inject=["ShoppingListCheckOffService"];
+  function ToBuyController(ShoppingListCheckOffService) {
     var toBuy = this;
     toBuy.toBuyList = [{ name: "cookies", quantity: 10 },
       { name: "cake", quantity: 10 },
@@ -11,14 +21,16 @@ angular.module('ShoppingListCheckOff', [])
     toBuy.buyItem = function(index){
       ShoppingListCheckOffService.moveToBought(index);
     };
-  })
-  .controller('AlreadyBoughtController', function(ShoppingListCheckOffService) {
+  }
+
+  AlreadyBoughtController.$inject=["ShoppingListCheckOffService"];
+  function AlreadyBoughtController(ShoppingListCheckOffService) {
     var bought = this;
     bought.boughtList = ShoppingListCheckOffService.boughtList;
 
+  }
 
-  })
-  .service('ShoppingListCheckOffService', function() {
+  function ShoppingListCheckOffService() {
     var service = this;
     service.toBuyList = [];
     service.boughtList = [];
@@ -27,5 +39,5 @@ angular.module('ShoppingListCheckOff', [])
       service.boughtList.push(service.toBuyList[index]);
       service.toBuyList.splice(index,1);
     };
-
-  });
+  }
+})();
